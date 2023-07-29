191e9ad4897e40e09361df11a84e8cc4948d150f
preetlakra <lakraaman007@gmail.com>
2023-07-29 20:29:08 +0530

    Update

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,3 +1,13 @@
 import CustomButton from "./CustomButton";
+import AiPicker from "./AiPicker";
+import colorPicker from "./colorPicker";
+import FilePicker   from "./FilePicker";
+import Tab from "./Tab";
 
-export { CustomButton };
+export { 
+    CustomButton,
+    AiPicker,
+    colorPicker,
+    FilePicker,
+    Tab,
+};
--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -3,6 +3,7 @@ import {motion, AnimatePresence} from 'framer-motion';
 import { useSnapshot } from 'valtio';
 
 import state from '../store';
+import { CustomButton } from '../components';
 import { 
 
     headContainerAnimation,
@@ -40,7 +41,10 @@ const Home = () => {
                                 Create your <strong>unique</strong> and <strong>exclusive</strong> shirt with our brand-new 3D customization tool. <strong>Your Imagination will become reality</strong>
                             </p>
                             <CustomButton 
-                                
+                                type = "filled"
+                                title ="Customize It"
+                                handleClick = {() => state.intro = false}
+                                customStyles="w-fit px-4 py-2.5 font-bold text-sm"
                             
                             />
                         </motion.div>
